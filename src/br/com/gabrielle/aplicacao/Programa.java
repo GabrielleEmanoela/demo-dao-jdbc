25b50d6a34260bc513e609e933511148cdeb45b3
Gabrielle Silva <biela.a09@gmail.com>
2021-02-04 07:36:36 -0300

    Atualizar dados.

--- src/br/com/gabrielle/aplicacao/Programa.java
+++ src/br/com/gabrielle/aplicacao/Programa.java
@@ -2,9 +2,7 @@ package br.com.gabrielle.aplicacao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.text.ParseException;
 
 import br.com.gabrielle.db.DB;
@@ -17,38 +15,24 @@ public class Programa {
 		PreparedStatement st = null;
 
 		try {
-
 			conn = DB.getConnection();
-			/*
-			 * st = conn.prepareStatement(
-			 * 
-			 * "INSERT INTO cliente " + "(nome, idade, endereco)" + "VALUES" + "(?,?,?)",
-			 * Statement.RETURN_GENERATED_KEYS
-			 * 
-			 * ); st.setString(1, "Gabrielle"); st.setInt(2, 555); st.setString(3,
-			 * "Av Planalto Jardim bela vista");
-			 */
+			
+			st = conn.prepareStatement("UPDATE cliente " + "SET idade = idade + ? " // Selecionando
+					+ "WHERE " // Restrição na atualização
+					+ "(ultimacompra = ?)");
 
-			st = conn.prepareStatement("insert into cliente (nome) values('Gabrielle'),('Silva')",
-					Statement.RETURN_GENERATED_KEYS);
+			st.setDouble(1, 5);
+			st.setInt(2, 10);
+
 			int rowsAffected = st.executeUpdate();
-
-			if (rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-				while (rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Pronto Id= " + id);
-
-				}
-			} else {
-				System.out.println("Nennhuma linha foi alterada");
-			}
+			System.out.println("Pronto! linhas afetadas " + rowsAffected);
 
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			DB.fecharDeclaracao(st);
+
 			DB.closeConnection();
+			DB.fecharDeclaracao(st);
 		}
 
 	}
