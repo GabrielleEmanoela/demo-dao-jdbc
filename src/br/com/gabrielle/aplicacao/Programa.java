2a6eb26c622a2de5343b1f8e5d7dadbec565bccf
Gabrielle Silva <biela.a09@gmail.com>
2021-02-04 11:37:38 -0300

    Adicionando Classe Departamento ao meu projeto

--- src/br/com/gabrielle/aplicacao/Programa.java
+++ src/br/com/gabrielle/aplicacao/Programa.java
@@ -12,37 +12,9 @@ public class Programa {
 
 	public static void main(String[] args) throws ParseException {
 
-		Connection conn = null;
-		Statement st = null;
+		br.com.gabrielle.modelo.entidades.Departamento obj = new br.com.gabrielle.modelo.entidades.Departamento(1, "Books");
 
-		try {
-			conn= DB.getConnection();
-			conn.setAutoCommit(false);
-			st = conn.createStatement();
-
-			int linhas1 = st.executeUpdate("UPDATE cliente SET ultimacompra = 100  WHERE idade = 0 ");
-			// int x = 1;
-			//// if (x < 2) {
-			// throw new SQLException("ERRO");
-			// }
-			int linhas2 = st.executeUpdate("UPDATE cliente SET ultimacompra = 30  WHERE idade = 30 ");
-			conn.commit();
-			System.out.println("Linha 1" + linhas1);
-			System.out.println("Linha 2" + linhas2);
-
-		} catch (SQLException e) {
-			try {
-				conn.rollback();
-				throw new DbExcecao(e.getMessage());
-			} catch (SQLException e1) {
-				throw new DbExcecao(e1.getMessage());
-
-			}
-		} finally {
-
-			DB.closeConnection();
-			DB.fecharDeclaracao(st);
-		}
+		System.out.println(obj);
 
 	}
 }
