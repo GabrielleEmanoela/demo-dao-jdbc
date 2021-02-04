ab7da5d6186ba15b9ef4879c07e724cf379888bb
Gabrielle Silva <biela.a09@gmail.com>
2021-02-04 08:48:22 -0300

    Transações.

--- src/br/com/gabrielle/aplicacao/Programa.java
+++ src/br/com/gabrielle/aplicacao/Programa.java
@@ -1,33 +1,43 @@
 package br.com.gabrielle.aplicacao;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 
 import br.com.gabrielle.db.DB;
-import br.com.gabrielle.db.DbIntegrityException;
+import br.com.gabrielle.db.DbExcecao;
 
 public class Programa {
 
 	public static void main(String[] args) throws ParseException {
 
 		Connection conn = null;
-		PreparedStatement st = null;
+		Statement st = null;
 
 		try {
-			conn = DB.getConnection();
+			conn= DB.getConnection();
+			conn.setAutoCommit(false);
+			st = conn.createStatement();
 
-			st = conn.prepareStatement("DELETE FROM cliente " + "WHERE " // comando de restricao
-					+ "ultimacompra = ?");
-
-			st.setDouble(1, 0);
-
-			int rowsAffected = st.executeUpdate();
-			System.out.println("Pronto! linhas afetadas " + rowsAffected);
+			int linhas1 = st.executeUpdate("UPDATE cliente SET ultimacompra = 100  WHERE idade = 0 ");
+			// int x = 1;
+			//// if (x < 2) {
+			// throw new SQLException("ERRO");
+			// }
+			int linhas2 = st.executeUpdate("UPDATE cliente SET ultimacompra = 30  WHERE idade = 30 ");
+			conn.commit();
+			System.out.println("Linha 1" + linhas1);
+			System.out.println("Linha 2" + linhas2);
 
 		} catch (SQLException e) {
-			throw new DbIntegrityException(e.getMessage());
+			try {
+				conn.rollback();
+				throw new DbExcecao(e.getMessage());
+			} catch (SQLException e1) {
+				throw new DbExcecao(e1.getMessage());
+
+			}
 		} finally {
 
 			DB.closeConnection();
